5ccb480764078c7ffb212f0a05b10e1b4e55e227
tomaspalica <tomasss.palica@gmail.com>
2026-04-15 17:46:51 +0200

    add header

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import posthtml from "@vituum/vite-plugin-posthtml";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [posthtml()],
   base: "/Filmoteka/",
 });
